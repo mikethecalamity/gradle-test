63bdbc8afafbcc4326061a2496a2281181d90afa
mweber <mweber03@villanova.edu>
2023-01-15 22:49:20 -0500

    Added cyclonedx

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,22 @@
+buildscript {
+  repositories {
+    mavenLocal()
+    maven {
+      url = uri("https://plugins.gradle.org/m2/")
+    }
+  }
+  dependencies {
+    classpath("org.cyclonedx:cyclonedx-gradle-plugin:1.7.3")
+  }
+}
+
 subprojects {
     apply(plugin = "java")
+    apply(plugin = "org.cyclonedx.bom")
     group = "org.dummy.utils"
     version = "0.0.1-SNAPSHOT"
     extensions.configure(JavaPluginExtension::class) {
-        sourceCompatibility = org.gradle.api.JavaVersion.VERSION_1_8
+        sourceCompatibility = org.gradle.api.JavaVersion.VERSION_17
     }
     tasks.withType(Test::class){
         useJUnitPlatform()
